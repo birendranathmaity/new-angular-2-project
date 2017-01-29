4b2b00a17c798cd15523d27cb0a845ea8c3a38b3
Birendranath Maity <birendranathmaity@gmail.com>
2017-01-29 22:04:51 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { Router } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import { AppComponent }            from './app.component';
+import { AppRoutingModule }        from './app-routing.module';
+import { CardGameComponent } from './card-game/card-game.component';
+import { FavoriteMovieComponent } from './favorite-movie/favorite-movie.component';
+import { MovieCastDetailsComponent } from './movie-cast-details/movie-cast-details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+   
+    
+    AppRoutingModule,
+    MaterialModule.forRoot()
   ],
+  declarations: [AppComponent,CardGameComponent,FavoriteMovieComponent,MovieCastDetailsComponent, HomeComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  
+  constructor(router: Router) {
+   
+  }
+}
+
