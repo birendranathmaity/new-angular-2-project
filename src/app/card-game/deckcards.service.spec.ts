a5c2c89398f064ced1709226bab8a1db2d0becd6
Birendranath Maity <birendranathmaity@gmail.com>
2017-01-30 14:53:36 +0530

    Update deckcards.service.spec.ts

--- src/app/card-game/deckcards.service.spec.ts
+++ src/app/card-game/deckcards.service.spec.ts
@@ -1,5 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
-import { Http } from '@angular/http';
+import { HTTP_PROVIDERS } from '@angular/http';
+import {DeckcardsService} from './deckcards.service';
+describe('Service: DeckcardsService', () => {
+  let service;
+  
+  //setup
+  beforeEachProviders(() => [
+    HTTP_PROVIDERS, 
+    DeckcardsService
+  ]);
+  
+  beforeEach(inject([DeckcardsService], s => {
+    service = s;
+  }));
+  
+  //specs
+  it('should return available cards', done => {
+    service.get().subscribe(x => { 
+      expect(x).toContain('en');
+      expect(x).toContain('es');
+      expect(x).toContain('fr');
+      expect(x.length).toEqual(3);
+      done();
+    });
+  });
+}) 
 
